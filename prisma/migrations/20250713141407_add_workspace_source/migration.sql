72133c7204ccb207926166528b41367fbffb9b92
MrLeritaite <mrleritaite@gmail.com>
2025-08-03 12:25:45 +0300

    test

--- prisma/migrations/20250713141407_add_workspace_source/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "WorkspaceSource" AS ENUM ('manual', 'github', 'gitlab');
-
--- AlterTable
-ALTER TABLE "Workspace" ADD COLUMN     "source" "WorkspaceSource" NOT NULL DEFAULT 'manual';
